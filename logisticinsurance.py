ee54caa0288bff9125dad2f547dee6cf1e3f62ce
YashwantKora <yashwantqwerty@gmail.com>
2025-11-24 10:27:25 +0530

    DAY 1/short

--- logisticinsurance.py
+++ logisticinsurance.py
@@ -3,29 +3,27 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+# Load
 df = pd.read_csv(r"C:\Users\yashw\OneDrive\Desktop\hhhhhh\python\datasets\insurance_data.csv")
-print(df)
-print(df.info())
 
-inputs = df.drop("bought_insurance", axis=1)
-outputs = df.drop("age", axis=1)
-print(inputs)
-print(outputs)
+# Split inputs & outputs
+inputs = df["age"]              # feature
+outputs = df["bought_insurance"]  # target
 
+# Plot
 plt.scatter(inputs, outputs)
 plt.xlabel("Age")
 plt.ylabel("Bought Insurance")
 plt.show()
 
-x_train,x_test,y_train,y_test=train_test_split(inputs,outputs,test_size=0.8)
-print(x_train)
-print(x_test)
-print(y_train)
-print(y_test)
+# Train-test split
+x_train, x_test, y_train, y_test = train_test_split(inputs, outputs, test_size=0.2)
 
-model=LogisticRegression()
-model.fit(x_train,y_train)
-y_pred=model.predict(x_test)
-print(y_pred)
-print(x_test)
+# Logistic Regression model
+model = LogisticRegression()
+model.fit(x_train.values.reshape(-1,1), y_train)
 
+# Predict
+y_pred = model.predict(x_test.values.reshape(-1,1))
+print("Predicted:", y_pred)
+print("Actual:", y_test.values)
